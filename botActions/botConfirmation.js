60ac2b8da1ccef9e97c4d33a47983759aab918f1
AkaHitsuji <yang025@e.ntu.edu.sg>
2019-09-28 14:53:48 +0530

    confirmation api calls

--- botActions/botConfirmation.js
+++ botActions/botConfirmation.js
@@ -1,5 +1,9 @@
+const axios = require("axios");
+
 const fbFunc = require('../firebaseFunctions.js');
 const { notStartedError, ERROR_MESSAGE } = require('./constants');
+
+const FAILED_GET_REQ = "Your confirmation message failed to send, please check your internet connection."
 
 module.exports.botYesCommand = (bot, db) => {
   bot.command('YES', ctx => {
@@ -10,7 +14,18 @@ module.exports.botYesCommand = (bot, db) => {
       .then((data) => {
         const { chatID } = data;
         if (typeof chatID === 'number') {
-          console.log('send yes to weijian');
+          console.log('yes api call made');
+          const yes_url = "https://jeevani-backend.herokuapp.com/chatbot/"+username+"/yes"
+          getResponse(yes_url)
+            .then((data) => {
+              const {message} = data
+              console.log(message);
+              ctx.reply(message)
+            })
+            .catch(error => {
+              console.log(error);
+              ctx.reply(FAILED_GET_REQ)
+            })
         }
       })
       .catch(error => {
@@ -29,7 +44,18 @@ module.exports.botNoCommand = (bot, db) => {
       .then((data) => {
         const { chatID } = data;
         if (typeof chatID === 'number') {
-          console.log('send no to weijian');
+          console.log('no api call made');
+          const no_url = "https://jeevani-backend.herokuapp.com/chatbot/"+username+"/no"
+          getResponse(no_url)
+            .then((data) => {
+              const {message} = data
+              console.log(message);
+              ctx.reply(message)
+            })
+            .catch(error => {
+              console.log(error);
+              ctx.reply(FAILED_GET_REQ)
+            })
         }
       })
       .catch(error => {
@@ -37,4 +63,16 @@ module.exports.botNoCommand = (bot, db) => {
         ctx.reply(ERROR_MESSAGE);
       });
   });
+};
+
+const getResponse = async url => {
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 };
